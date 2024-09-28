637b28b151b66382e7c4dc7528a849360feb14ad
Sanjana Shivananda <sanjana@sanjanas-air.dhcp.asu.edu>
2024-09-28 15:00:16 -0700

    Login

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import './Login.css'
+
+
+function Login() {
+    const CLIENT_ID = "181e3f2477f14d68a0b880b607b0e366"
+    const REDIRECT_URI = "http://localhost:3000"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+
+    const [token, setToken] = useState("")
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    return (
+        <div className="Login">
+            <div className="login-page">
+                <h1>Spotify React</h1>
+                {!token ?
+                    <button><a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                        to Spotify</a></button>
+                    : <button onClick={logout}>Logout</button>}
+            </div>
+        </div>
+    )
+}
+
+export default Login
